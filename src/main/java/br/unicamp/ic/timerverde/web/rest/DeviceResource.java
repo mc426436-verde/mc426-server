c5d581c855b58d75c4c0c30d64e073c1685db7ef
Thiago Pinto <thspinto@gmail.com>
2016-05-30 23:31:25 -0300

    Não altera o status do devices se requisição para o arduino falhar

--- src/main/java/br/unicamp/ic/timerverde/web/rest/DeviceResource.java
+++ src/main/java/br/unicamp/ic/timerverde/web/rest/DeviceResource.java
@@ -138,10 +138,12 @@ public class DeviceResource {
             } else {
                 result.setStatus(DeviceStatusEnum.OFF);
             }
-            deviceRepository.save(result);
-            updateArduino(result);
+            if(updateArduino(result)) {
+                deviceRepository.save(result);
+            }
         });
 
+        optionalDevice = Optional.ofNullable(deviceRepository.findOneWithEagerRelationships(id));
         return optionalDevice
             .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
             .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -175,17 +177,19 @@ public class DeviceResource {
             .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    private void updateArduino(Device device) {
+    private boolean updateArduino(Device device) {
         try {
             String ip = this.env.getProperty("dino.arduino.ip");
             String port = this.env.getProperty("dino.arduino.port");
             Socket clientSocket = new Socket(ip, Integer.valueOf(port));
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-            outToServer.writeBytes(device.getId() + ":" + device.getStatus().value());
+            outToServer.writeBytes(device.getId() + ":" + device.getStatus().value() + '\n');
             clientSocket.close();
         } catch (IOException e) {
             log.error("Failed to update arduino");
+            return false;
         }
+        return true;
     }
 
 
